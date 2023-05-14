6ec45c73d4293a142df451b5e74fd11b8584d13e
Excelsiuz <129679853+Excelsiuz@users.noreply.github.com>
2023-05-14 22:03:36 +0700

    Add files via upload

---
+++ routes/AboutUs.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/900x400"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-5">
+          <br></br>
+            <p>
+            Selamat datang di Railenz, platform jual beli properti terpercaya dan terdepan untuk memenuhi kebutuhan investasi dan hunian Anda. Kami adalah tujuan utama bagi para pencari properti yang mencari transaksi yang aman, efisien, dan terpercaya.
+</p>
+<br></br>
+           <p> Railenz menyediakan berbagai macam pilihan properti yang lengkap, mulai dari apartemen, rumah, kondominium, vila, dan tanah. Kami bekerja sama dengan pengembang properti terkemuka dan agen properti profesional untuk memastikan Anda mendapatkan akses ke properti terbaik di pasar.
+</p><br></br>
+           <p> Dengan menggunakan teknologi canggih dan platform yang mudah digunakan, kami menyediakan fitur pencarian properti yang akurat dan lengkap. Anda dapat dengan mudah menemukan properti yang sesuai dengan preferensi Anda, seperti lokasi, tipe properti, harga, dan fasilitas yang Anda inginkan.
+</p><br></br>
+            <p>Railenz juga memberikan kemudahan dalam proses jual beli properti. Kami menyediakan sistem yang transparan, memudahkan Anda dalam negosiasi harga, pemesanan, dan pembayaran. Tim kami yang berpengalaman siap memberikan dukungan penuh mulai dari pertanyaan seputar properti hingga proses penyelesaian transaksi.
+</p><br></br>
+           <p> Keamanan adalah prioritas utama kami. Kami melindungi informasi pribadi Anda dengan sistem keamanan yang canggih dan menjaga kerahasiaan data Anda selama proses transaksi. Kami juga memverifikasi setiap properti yang diiklankan untuk memastikan keabsahan dan keakuratan informasi yang diberikan.
+</p><br></br>
+            <p>Railenz tidak hanya menyediakan platform jual beli properti, tetapi juga merupakan sumber informasi properti yang berharga. Melalui blog dan artikel kami, Anda dapat memperoleh tips dan panduan dalam membeli, menjual, atau berinvestasi di properti.
+</p><br></br>
+           <p> Bergabunglah dengan Railenz sekarang dan temukan properti impian Anda. Kami berkomitmen untuk memberikan pengalaman jual beli properti yang terbaik, memudahkan Anda dalam mencapai tujuan properti Anda, dan memberikan nilai tambah bagi masa depan investasi Anda.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
